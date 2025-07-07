9c290c9439a37c43785fe50441f3c28b92dade80
shivani <shivaniw69@gmail.com>
2025-07-07 22:42:56 +0530

    first commit

--- Alarm.project/script.js
+++ Alarm.project/script.js
@@ -1,4 +1,37 @@
 
+// let stp;
+// let sett=()=>{
+//     let inputtime=document.querySelector("#curtime").value   //4:53
+//     let dispaly=document.querySelector("#show")
+//     let ringg=document.querySelector("#music")
+//     stp=setInterval(()=>{
+//         let time=new Date()
+//         let currenttime=`${time.getHours().toString().padStart(2,"0")}:${time.getMinutes().toString().padStart(2,"0")}` //4:
+//         if(currenttime==inputtime)
+//         {
+//             dispaly.innerHTML="Alarm is ringing...."
+//             dispaly.style.color="darkgoldenrod"
+//              ringg.play()
+//         }
+//         else
+//         {
+//             dispaly.innerHTML="Alarm is set.."
+//         }
+
+    
+//      },1000)
+
+    
+// }
+
+//     //   let stop=()=>{
+//     //      setTimeout(()=>{
+//     //      clearInterval(stp)
+//     //     ringg.pause()
+//     //         })
+//     //     }
+
+
 let stp;
 let sett=()=>{
     let inputtime=document.querySelector("#curtime").value   //4:53
@@ -30,4 +63,6 @@ let sett=()=>{
     //     ringg.pause()
     //         })
     //     }
+        
+
         
