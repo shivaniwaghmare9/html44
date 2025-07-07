f6c350fc41c08ac070c92edcf216b6f3cae462cb
shivani <shivaniw69@gmail.com>
2025-07-07 22:46:47 +0530

    first commit

--- Calculator.project/Script.js
+++ Calculator.project/Script.js
@@ -1,5 +1,50 @@
 
 
+// //    let store=""
+// //  let cal=(val)=>{
+// //       store+=val
+// //  document.querySelector("#display").value+="";
+// // // show.innerHTML=(store)
+// //  }
+// //  let equal=()=>{
+// //     let show=document.querySelector("#display").value
+// //     show.innerHTML=eval(store)
+// //     store=eval(store)
+// //     store.toString()
+// // }
+// // let AC=()=>{
+// //     store=""
+// //     let show=document.querySelector("#display").value
+// //     show.innerHTML=""
+// // }
+// // let del=()=>{
+// //     store=store.slice(0,-1)
+// //     let show=document.querySelector("#display").value
+// //     show.innerHTML=store
+// // }
+// let SQ=()=>{
+   
+//     let show=document.querySelector("#display")
+//     show.value=Math.sqrt(display.value)
+// }
+// let sinf=()=>{
+//     let show=document.querySelector("#display")
+// show.value=Math.sin(display.value)
+
+// }
+// let tanf=()=>{
+//     let show=document.querySelector("#display")
+// show.value=Math.tan(display.value)
+
+// }
+// let cosf=()=>{
+//     let show=document.querySelector("#display")
+// show.value=Math.cos(display.value)
+
+// }
+
+
+
 //    let store=""
 //  let cal=(val)=>{
 //       store+=val
@@ -42,3 +87,4 @@ let cosf=()=>{
 show.value=Math.cos(display.value)
 
 }
+
