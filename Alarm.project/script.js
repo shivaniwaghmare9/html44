af83a0ff5d457f40cba894156e0726ec6ab78de1
shivani <shivaniw69@gmail.com>
2025-09-19 23:14:49 +0530

    js

--- Alarm.project/script.js
+++ Alarm.project/script.js
@@ -31,3 +31,36 @@ let sett=()=>{
     //         })
     //     }
         
+
+let stp;
+let sett=()=>{
+    let inputtime=document.querySelector("#curtime").value   //4:53
+    let dispaly=document.querySelector("#show")
+    let ringg=document.querySelector("#music")
+    stp=setInterval(()=>{
+        let time=new Date()
+        let currenttime=`${time.getHours().toString().padStart(2,"0")}:${time.getMinutes().toString().padStart(2,"0")}` //4:
+        if(currenttime==inputtime)
+        {
+            dispaly.innerHTML="Alarm is ringing...."
+            dispaly.style.color="darkgoldenrod"
+             ringg.play()
+        }
+        else
+        {
+            dispaly.innerHTML="Alarm is set.."
+        }
+
+    
+     },1000)
+
+    
+}
+
+    //   let stop=()=>{
+    //      setTimeout(()=>{
+    //      clearInterval(stp)
+    //     ringg.pause()
+    //         })
+    //     }
+        
