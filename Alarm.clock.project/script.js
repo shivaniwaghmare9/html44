3ee2ec608880b06a0590b76835f895c671a73fb5
shivani <shivaniw69@gmail.com>
2026-03-18 23:21:19 +0530

    .,.,.

--- Alarm.clock.project/script.js
+++ Alarm.clock.project/script.js
@@ -25,7 +25,7 @@
 
      }   
      function alarmSetFunction() {
-	let now = new Date();
+	  let now = new Date();
 	let selectedDate = new Date(dateInput.value + "T" + tInput.value);
 	if (selectedDate <= now) {
 		alert(`Invalid time. Please select 
